e8f2483d434b678c6374db2f7ab326b8c6a6a982
Priyanka_Mangpahang <priyankamangpahang@gmail.com>
2026-02-27 16:02:39 +0545

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,31 @@
+let a = 10;
+let b = 5;
+
+// Add
+let add = a + b;
+
+// Subtract
+let subtract = a - b;
+
+// Multiply
+let multiply = a * b;
+
+// Divide
+let divide = a / b;
+
+// Increment
+a++;
+
+// Decrement
+b--;
+
+// Remainder
+let remainder = a % b;
+
+console.log("Addition:", add);
+console.log("Subtraction:", subtract);
+console.log("Multiplication:", multiply);
+console.log("Division:", divide);
+console.log("Incremented a:", a);
+console.log("Decremented b:", b);
+console.log("Remainder:", remainder);
